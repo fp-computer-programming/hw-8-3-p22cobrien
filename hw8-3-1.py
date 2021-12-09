1570fb323c55e8ea52b2f96c12310bf5b94ed6ac
p22cobrien <p22cobrien@fairfieldprep.org>
2021-12-09 10:37:38 -0500

    submit hw 8-3.py

--- hw8-3-1.py
+++ hw8-3-1.py
@@ -2,16 +2,17 @@
 
 def count_odds(num):
     total = 0
-    z = 0
     x = 0
-    while x in range(z):
-        z in  num
-        if x % 2 != 0:
+    while x < len(num):
+        if num[x] % 2 != 0:
             total += 1
+            x += 1
         else:
-            continue
-        x += 1
+            x += 1
     return total
 
-
-print(count_odds([1, 2, 3, 4, 5, 6]))
+ 
+print(count_odds([1, 2, 3, 4, 5, 6]) == 3)
+print(count_odds([1, 2, 3, 4, 5, 6]) == 3)
+print(count_odds([1, 3, 5, 7, 9]) == 5)
+print(count_odds([2, 4, 6, 8, 10]) == 0)
--- hw8-3-2.py
+++ hw8-3-2.py
@@ -2,13 +2,17 @@
 
 def sum_no_odd(num):
     total = 0
-    trig = True
-    while trig == True:
-        for x in num:
-            if x % 2 != 0:
-                break
-            else:
-               total += x
+    x = 0
+    z = 0
+    while x < len(num):
+        if num[x] % 2 == 0:
+            total += num[x]
+            x += 1
+        else:
+            x += len(num) + 9
     return total
 
 print(sum_no_odd([2, 4, 5]))
+print(sum_no_odd([2, 4, 6, 8, 9]) == 20)
+print(sum_no_odd([13, 12, 10]) == 0)
+print(sum_no_odd([14, 16, 32]) == 62)
